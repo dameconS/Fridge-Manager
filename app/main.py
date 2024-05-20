917e6d73a7706e31a353f7e586a6eaf9f4f87b61
SHIN MINHO <damecon@icloud.com>
2024-05-20 16:56:15 +0900

    delete ocr

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException, Depends, FastAPI, UploadFile
 
 from sqlalchemy.orm import Session
 
-from . import crud, models, schemas, scanner
+from . import crud, models, schemas
 from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -30,28 +30,28 @@ def read_processed_foods(processed_food_name: str, db: Session = Depends(get_db)
     processed_foods = crud.get_processed_food(db=db, processed_food_name=processed_food_name)
     return processed_foods
 
-@app.post("/ocr/")
-async def ocr(file: UploadFile, db: Session = Depends(get_db)):
-    try:
-        contents = await file.read()
-        UPLOAD_DIR = "app/receipt"
-        filename = f"receipt.jpg"
-        with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
-            fp.write(contents)
-        name_list = scanner.ocr()
-        result = name_list
+# @app.post("/ocr/")
+# async def ocr(file: UploadFile, db: Session = Depends(get_db)):
+#     try:
+#         contents = await file.read()
+#         UPLOAD_DIR = "app/receipt"
+#         filename = f"receipt.jpg"
+#         with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
+#             fp.write(contents)
+#         name_list = scanner.ocr()
+#         result = name_list
 
-        # 음식 이름 리스트를 순회하면서 데이터베이스 조회
-        for food_name in name_list:
-            foods = crud.get_food(db=db, food_name=food_name)
-            result.extend(foods)
+#         # 음식 이름 리스트를 순회하면서 데이터베이스 조회
+#         for food_name in name_list:
+#             foods = crud.get_food(db=db, food_name=food_name)
+#             result.extend(foods)
 
-        # 처리된 음식 정보도 조회
-        for processed_food_name in name_list:
-            processed_foods = crud.get_processed_food(db=db, processed_food_name=processed_food_name)
-            result.extend(processed_foods)
+#         # 처리된 음식 정보도 조회
+#         for processed_food_name in name_list:
+#             processed_foods = crud.get_processed_food(db=db, processed_food_name=processed_food_name)
+#             result.extend(processed_foods)
 
-        return result
+#         return result
     
-    except Exception as exc:
-        raise HTTPException(status_code=500, detail=str(exc))
+#     except Exception as exc:
+#         raise HTTPException(status_code=500, detail=str(exc))
